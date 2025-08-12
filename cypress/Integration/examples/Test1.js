63ed1630c7b59824a355460cff7eb3344754a277
Poulami Datta <poulami92datta@gmail.com>
2025-08-12 08:38:37 +0530

    Get,Find,Contains,Eq usage

--- cypress/Integration/examples/Test1.js
+++ cypress/Integration/examples/Test1.js
@@ -1,8 +1,24 @@
 //cypress - Spec
 
-describe('Dummy Test Suite', () => {
-  it('should pass this dummy test', () => {
-    expect(true).to.equal(true);
-  });
-});
+describe('My First Test Suite', () => {
+
+    it('My First Test Case', () => {
+        cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/')
+        cy.get('input.search-keyword').type('ca')
+        cy.wait(2000)
+        //find matching visible elements only
+        cy.get('.product:visible').should('have.length',4)
+
+        //find child elements within parent element
+        cy.get('.products').find('.product').should('have.length',4)
+
+        // Get the DOM element containing the text
+        cy.get('.products').find('.product').eq(1).contains('ADD TO CART').click()
+
+        
+
+    })
+    
+ })
+
 
