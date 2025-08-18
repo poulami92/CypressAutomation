9e346171c0b0c068d15e9b6b29a07b54d2a9ac87
Poulami Datta <poulami92datta@gmail.com>
2025-08-18 19:13:32 +0530

    Handle Child Window

---
+++ cypress/Integration/examples/HandleChildTab.js
@@ -0,0 +1,35 @@
+/// <reference types="Cypress" />
+
+describe('My Second Test Suite', () => {
+
+    it('My Second Test Case', () => {
+        cy.visit('https://rahulshettyacademy.com/AutomationPractice/')
+
+        // cy.get('#opentab').click()   // open tab in separate browser tab 
+ 
+        cy.get('#opentab').invoke('removeAttr','target').click()  // open tab in same browser window
+        
+        cy.origin('https://www.qaclickacademy.com/', () =>{
+
+            cy.contains('About us').click()
+
+            cy.get('.page-banner-cont h2').then ( ($txt) =>{
+
+                const text = $txt.text()
+                expect(text).to.equal('About Us')
+
+            })
+
+            cy.get('.page-banner-cont h2').should('have.text','About Us')
+
+            cy.get('.page-banner-cont h2').should('contain','About Us')
+
+        })
+        
+        
+
+    })
+    
+ })
+
+
